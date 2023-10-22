d6f4bdc7a59a9611170aa73d374979bbc14d35bb
PX-72 <zoli.konyves@pm.me>
2023-10-22 19:46:46 +0100

    minor change

--- src/main/java/com/leapfrog/bff/models/MarketDataRequestConfiguration.java
+++ src/main/java/com/leapfrog/bff/models/MarketDataRequestConfiguration.java
@@ -1,12 +1,11 @@
 package com.leapfrog.bff.models;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public record MarketDataRequestConfiguration (
         String configurationId,
         Integer size,
         Integer intervalInMillis,
-        Integer intervalDelayInMillis,
+        Integer initialDelayInMillis,
         List<String> ccyFilter
 ){}
